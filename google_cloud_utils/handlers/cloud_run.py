e97f2b4ceb53ad27493b47165e5e25cadd8b0eab
Pierre Denig <pierre@datagem-consulting.com>
2026-06-02 23:59:02 +0200

    Update CloudRunJobHandler to use project_id from service account or default credentials; bump version to 0.3.1

--- google_cloud_utils/handlers/cloud_run.py
+++ google_cloud_utils/handlers/cloud_run.py
@@ -1,7 +1,9 @@
+import google.auth
 from google.oauth2 import service_account
 from google.cloud import run_v2
 
-_JOB_PATH = "projects/skilled-sunrise-447019-e2/locations/europe-west9/jobs/{job_name}"
+_DEFAULT_LOCATION = "europe-west9"
+_JOB_PATH = "projects/{project_id}/locations/{location}/jobs/{job_name}"
 
 
 class CloudRunJobHandler:
@@ -13,15 +15,28 @@ class CloudRunJobHandler:
                 serviceAccountJson,
                 scopes=["https://www.googleapis.com/auth/cloud-platform"],
             )
+            self.project_id = serviceAccountJson.get("project_id") or credentials.project_id
             self.client = run_v2.JobsClient(credentials=credentials)
         else:
-            self.client = run_v2.JobsClient()
+            credentials, project_id = google.auth.default(
+                scopes=["https://www.googleapis.com/auth/cloud-platform"],
+            )
+            self.project_id = project_id
+            self.client = run_v2.JobsClient(credentials=credentials)
 
-    def trigger_job(self, job_name: str, env: dict) -> str:
-        """Trigger a Cloud Run Job execution. Returns the execution name (non-blocking)."""
+    def trigger_job(self, job_name: str, env: dict, location: str = _DEFAULT_LOCATION) -> str:
+        """Trigger a Cloud Run Job execution. Returns the execution name (non-blocking).
+
+        The job's project is taken from the service account's ``project_id`` (or
+        the ambient credentials' project when no SA was supplied).
+        """
         operation = self.client.run_job(
             request=run_v2.RunJobRequest(
-                name=_JOB_PATH.format(job_name=job_name),
+                name=_JOB_PATH.format(
+                    project_id=self.project_id,
+                    location=location,
+                    job_name=job_name,
+                ),
                 overrides=run_v2.RunJobRequest.Overrides(
                     container_overrides=[
                         run_v2.RunJobRequest.Overrides.ContainerOverride(
